0de5fac91ed3a3b60280eefcc5d72d1158b531e2
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-04-28 13:56:35 +0700

    create ChatFedd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./Components/ChatFeed";
 import "./App.css";
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
       projectID={process.env.REACT_APP_PROID}
       userName={process.env.REACT_APP_USERNAME}
       userSecret={process.env.REACT_APP_USERSECRET}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
 };
