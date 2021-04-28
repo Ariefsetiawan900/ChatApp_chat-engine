c07b574101ce9050fd61737352a63dd30b2b540f
Ariefsetiawan900 <ariefsetiawan8788@gmail.com>
2021-04-28 14:48:37 +0700

    create somthing file in folder components & styling ChatFeed.js

---
+++ src/Components/index.js
@@ -0,0 +1,6 @@
+import ChatFeed from "./ChatFeed";
+import MyMessage from "./MyMessage";
+import MessageForm from "./MessageForm";
+import TheirMessage from "./TheirMessage";
+
+export { ChatFeed, MyMessage, MessageForm, TheirMessage };
